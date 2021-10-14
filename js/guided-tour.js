ea20226b07855a7d0502e0b041d904f92dfdb101
Chris Beer <chris@cbeer.info>
2021-10-14 09:43:40 -0700

    Refactor guided tours to bring it more in line with slideshows

--- js/guided-tour.js
+++ js/guided-tour.js
@@ -2,83 +2,69 @@ import { Application, Controller } from "/js/stimulus.js"
 window.Stimulus = Application.start()
 
 Stimulus.register("guided-tour", class extends Controller {
-  static targets = [ "data", "slides", "bio", "caption", "initialScreen", "endScreen" ]
+  static targets = [ "slides", "caption", "slideContainer" ]
 
   connect() {
-    this.index = -1;
-    this.json = JSON.parse(this.dataTarget.innerHTML);
-  }
-
-  start() {
     this.index = 0;
-    this.initialScreenTarget.classList.add('d-none');
-    this.slidesTarget.classList.remove('d-none');
-    this.endScreenTarget.classList.add('d-none');
 
-    this.showCurrentItem();
-    viewer.viewport.goHome();
+    if (window.location.hash && this.slidesTargets.findIndex(x => x.id == window.location.hash.substring(1)) > 0) {
+      this.index = this.slidesTargets.findIndex(x => x.id == window.location.hash.substring(1));
+    }
+
+    this.render();
   }
 
+  // reset the slideshow back to the intro card
   reset() {
-    this.index = -1;
-    this.intialScreenTarget.classList.remove('d-none');
-    this.slidesTarget.classList.add('d-none');
-    this.endScreenTarget.classList.add('d-none');
+    this.setIndex(0);
   }
 
-  end() {
-    this.initialScreenTarget.classList.add('d-none');
-    this.slidesTarget.classList.add('d-none');
-    this.endScreenTarget.classList.remove('d-none');
-    this.captionTarget.classList.add('d-none');
-    viewer.viewport.goHome();
-  }
-
+  // paginate to the next slide, or the end card
   next() {
-    if ((this.index + 1) >= Object.entries(this.json).length) {
-      return this.end();
-    }
-
-    this.index++;
-    this.showCurrentItem();
+    this.setIndex(this.index + 1);
   }
 
+  // paginate to the previous slide, or the intro card
   previous() {
-    if (this.index <= 0) {
-      return this.reset();
-    }
-
-    this.index--;
-    this.showCurrentItem();
+    this.setIndex(this.index - 1);
   }
 
+  setIndex(index) {
+    this.index = Math.max(0, Math.min(index, this.slidesTargets.length));
+    this.render();
+  }
+
+  // @private
+  // get the currently displayed item
   getItem() {
-    const [key, value] = Object.entries(this.json)[this.index];
-
-    return value;
+    return this.slidesTargets[this.index];
   }
 
-  showCurrentItem() {
+  render() {
     const item = this.getItem(this.index);
 
-    if (item.viewport) {
-      viewer.viewport.panTo(viewer.viewport.imageToViewportCoordinates(item.viewport.x, item.viewport.y));
-      viewer.viewport.zoomTo(item.viewport.zoom);
+    this.slidesTargets.forEach(x => x.classList.add('d-none'));
+    item.classList.remove('d-none');
+
+    this.slideContainerTargets.forEach(container => {
+      if (container.contains(item)) {
+        container.classList.remove('d-none');
+      } else {
+        container.classList.add('d-none');
+      }
+    });
+
+    const data = JSON.parse(item.querySelector("script[type='application/json']")?.innerHTML || null) ?? {};
+
+    if (data.viewport) {
+      viewer.viewport.panTo(viewer.viewport.imageToViewportCoordinates(data.viewport.x, data.viewport.y));
+      viewer.viewport.zoomTo(data.viewport.zoom);
+    } else if (this.index == 0 || (this.index + 1) == this.slidesTargets.length) {
+      viewer.viewport.goHome();
     }
 
-    if (item.bio) {
-      this.bioTarget.classList.remove('d-none');
-      const card = document.createElement('div');
-      const title = document.createElement('h4');
-      title.innerHTML = item.bio.title;
-      card.prepend(title);
-      this.bioTarget.replaceChildren(card);
-    } else {
-      this.bioTarget.classList.add('d-none');
-    }
-
-    if (item.caption) {
-      this.captionTarget.innerHTML = item.caption;
+    if (data.caption) {
+      this.captionTarget.innerHTML = data.caption;
       this.captionTarget.classList.remove('d-none');
     } else {
       this.captionTarget.classList.add('d-none');
