33a51ae8acaa8b1a2387f9b0dbc0515d7b83d1cc
Chris Beer <chris@cbeer.info>
2021-10-20 09:09:53 -0700

    Auto-hide the modal after a timeout; part of #55

--- js/controllers/more-info.js
+++ js/controllers/more-info.js
@@ -1,18 +1,25 @@
 import { Controller } from "/js/stimulus.js"
 
 export default class extends Controller {
+  static timeout = 15*60*1000; // 15 minutes
   static targets = [ "modal" ]
 
   connect() {
   }
 
+  disconnect() {
+    if (this.autohideCallback) window.clearTimeout(this.autohideCallback);
+  }
+
   show() {
     this.dispatch("show");
     this.modalTarget.classList.remove('d-none');
+    this.autohideCallback = window.setTimeout(() => this.hide(), this.constructor.timeout);
   }
 
   hide() {
     this.dispatch("hide");
     this.modalTarget.classList.add('d-none');
+    if (this.autohideCallback) window.clearTimeout(this.autohideCallback);
   }
 }
