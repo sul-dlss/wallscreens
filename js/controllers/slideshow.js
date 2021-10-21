e71213b71ad5de6fb630a64d0258d526770a75e2
Chris Beer <chris@cbeer.info>
2021-10-21 08:50:54 -0700

    Wire up autoplay for guided tours and slideshows; fixes #52

--- js/controllers/slideshow.js
+++ js/controllers/slideshow.js
@@ -3,11 +3,41 @@ import { Controller } from "/js/stimulus.js"
 export default class extends Controller {
   static values = { index: { type: Number, default: 0 } }
   static targets = ["slides", "slideArea", "slideContainer" ]
+  static autoplayTimeout = 5 * 60 * 1000; // 5 minutes
+  static autoplayIntervalTime = 1 * 60 * 1000; // 1 minute per slide in autoplay mode
 
   connect() {
     if (window.location.hash && this.slidesTargets.findIndex(x => x.id == window.location.hash.substring(1)) > 0) {
       this.indexValue = this.slidesTargets.findIndex(x => x.id == window.location.hash.substring(1));
     }
+
+    this.resetAutoplayTimer();
+  }
+
+  disconnect() {
+    if (this.autoplayInterval) window.clearInterval(this.autoplayInterval);
+    if (this.autoplayTimer) window.clearTimeout(this.autoplayTimer);
+  }
+
+  // enter the auto-play mode where we cycle to the end of the slideshow and then go back to the
+  // intro slide
+  autoplay() {
+    if (this.autoplayInterval) window.clearInterval(this.autoplayInterval);
+
+    this.autoplayInterval = window.setInterval(() => {
+      if (this.indexValue == 0) return this.resetAutoplayTimer();
+      if (this.ended) return this.indexValue = 0;
+      this.indexValue = this.indexValue + 1
+    }, this.constructor.autoplayIntervalTime);
+  }
+
+  // (re)set a timer for entering the autoplay after an idle timeout  
+  resetAutoplayTimer() {
+    if (this.autoplayInterval) window.clearInterval(this.autoplayInterval);
+    if (this.autoplayTimer) window.clearTimeout(this.autoplayTimer);
+    if (this.indexValue == 0) return;
+
+    this.autoplayTimer = window.setTimeout(() => this.autoplay(), this.constructor.autoplayTimeout);
   }
 
   // reset the slideshow back to the intro card
@@ -18,11 +48,17 @@ export default class extends Controller {
   // paginate to the next slide, or the end card
   next() {
     this.indexValue = Math.min(this.indexValue + 1, this.slidesTargets.length - 1);
+    this.resetAutoplayTimer();
   }
 
   // paginate to the previous slide, or the intro card
   previous() {
     this.indexValue = Math.max(this.indexValue - 1, 0);
+    this.resetAutoplayTimer();
+  }
+
+  get ended() {
+    return this.indexValue == this.slidesTargets.length - 1;
   }
 
   // @private
