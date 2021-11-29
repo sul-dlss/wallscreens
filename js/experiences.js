41460d98446b629d3db1b4bc98cd9d4f23cbebbb
Nick Budak <budak@stanford.edu>
2021-11-29 14:49:05 -0800

    Add attract mode behavior for experience type index pages
    Fixes #264

--- js/experiences.js
+++ js/experiences.js
@@ -3,6 +3,7 @@ import GuidedTourExperience from "/js/controllers/guided-tour.js"
 import OralHistoryExperience from "/js/controllers/oral-history.js"
 import SlideshowExperience from "/js/controllers/slideshow.js"
 import MoreInfoModal from "/js/controllers/more-info.js"
+import AttractMode from "/js/controllers/attract-mode.js"
 
 window.Stimulus = Application.start()
 
@@ -10,3 +11,4 @@ Stimulus.register("guided-tour", GuidedTourExperience)
 Stimulus.register("oral-history", OralHistoryExperience)
 Stimulus.register("slideshow", SlideshowExperience)
 Stimulus.register("more-info", MoreInfoModal)
+Stimulus.register("attract-mode", AttractMode)
