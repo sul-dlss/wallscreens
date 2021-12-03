cfc535570fbca744e54f897ba89a00d311ac9afa
Cory Lown <corylown@stanford.edu>
2021-12-03 14:05:25 -0500

    Add Rubocop for linting Ruby code.

--- _plugins/git_info.rb
+++ _plugins/git_info.rb
@@ -1,4 +1,6 @@
+# frozen_string_literal: true
+
 # Make version information from git available via site.git_info in templates
-Jekyll::Hooks.register :site, :pre_render do |site, payload|
-    site.config['git_info'] = `git describe --tags`
+Jekyll::Hooks.register :site, :pre_render do |site, _payload|
+  site.config['git_info'] = `git describe --tags`
 end
