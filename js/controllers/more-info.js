7a76ca97c577ac245c309989003f3d52df5dd394
Cory Lown <corylown@stanford.edu>
2021-12-10 12:10:46 -0500

    Switch from standard to airbnb styleguide

--- js/controllers/more-info.js
+++ js/controllers/more-info.js
@@ -1,27 +1,28 @@
-import { Controller } from '/js/stimulus.js'
+import { Controller } from '/js/stimulus.js';
 
 export default class extends Controller {
   static timeout = 15 * 60 * 1000; // 15 minutes
-  static targets = ['modal']
 
-  connect () {
+  static targets = ['modal'];
+
+  connect() {
   }
 
-  disconnect () {
-    if (this.autohideCallback) window.clearTimeout(this.autohideCallback)
+  disconnect() {
+    if (this.autohideCallback) window.clearTimeout(this.autohideCallback);
   }
 
-  show () {
-    this.dispatch('show')
-    gtag('event', 'more-info:show')
-    this.modalTarget.hidden = false
-    this.autohideCallback = window.setTimeout(() => this.hide(), this.constructor.timeout)
+  show() {
+    this.dispatch('show');
+    gtag('event', 'more-info:show');
+    this.modalTarget.hidden = false;
+    this.autohideCallback = window.setTimeout(() => this.hide(), this.constructor.timeout);
   }
 
-  hide () {
-    this.dispatch('hide')
-    gtag('event', 'more-info:hide')
-    this.modalTarget.hidden = true
-    if (this.autohideCallback) window.clearTimeout(this.autohideCallback)
+  hide() {
+    this.dispatch('hide');
+    gtag('event', 'more-info:hide');
+    this.modalTarget.hidden = true;
+    if (this.autohideCallback) window.clearTimeout(this.autohideCallback);
   }
 }
--- js/experiences.js
+++ js/experiences.js
@@ -1,14 +1,14 @@
-import { Application } from '/js/stimulus.js'
-import GuidedTourExperience from '/js/controllers/guided-tour.js'
-import OralHistoryExperience from '/js/controllers/oral-history.js'
-import SlideshowExperience from '/js/controllers/slideshow.js'
-import MoreInfoModal from '/js/controllers/more-info.js'
-import AttractMode from '/js/controllers/attract-mode.js'
+import { Application } from '/js/stimulus.js';
+import GuidedTourExperience from '/js/controllers/guided-tour.js';
+import OralHistoryExperience from '/js/controllers/oral-history.js';
+import SlideshowExperience from '/js/controllers/slideshow.js';
+import MoreInfoModal from '/js/controllers/more-info.js';
+import AttractMode from '/js/controllers/attract-mode.js';
 
-window.Stimulus = Application.start()
+window.Stimulus = Application.start();
 
-Stimulus.register('guided-tour', GuidedTourExperience)
-Stimulus.register('oral-history', OralHistoryExperience)
-Stimulus.register('slideshow', SlideshowExperience)
-Stimulus.register('more-info', MoreInfoModal)
-Stimulus.register('attract-mode', AttractMode)
+Stimulus.register('guided-tour', GuidedTourExperience);
+Stimulus.register('oral-history', OralHistoryExperience);
+Stimulus.register('slideshow', SlideshowExperience);
+Stimulus.register('more-info', MoreInfoModal);
+Stimulus.register('attract-mode', AttractMode);
